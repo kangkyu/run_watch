1416efe0bc31b3e9485d31bea7808c9e5b561ee9
Kang Kyu Lee <kangkyu1111@gmail.com>
2014-07-15 13:07:20 -0700

    add basic will_paginate

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.order('date')
+    @tasks = Task.order('date').page(params[:page])
     @do_tasks = Task.where(completed: true).order('date')
     @un_tasks = Task.where(completed: false).order('date')
   end
