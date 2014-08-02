c9990a74ee2ac0af1c994c1f3543f193ee1d0f27
Kang Kyu Lee <kangkyu1111@gmail.com>
2014-08-01 22:22:37 -0700

    sessions controller adjust

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,12 +7,12 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user.id 
       Task.all.each_with_index do |task, i|
         if task.title != 'cancel'
           Status.create(user_id: @user.id, task_id: i+1, completed: false)
         end
       end
+      session[:user_id] = @user.id 
       redirect_to root_url, notice: "Thank you for signing up"
     else
       render "new"
