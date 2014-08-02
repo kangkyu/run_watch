aee764d417b3a3891fda4a16f417889b11698ae7
Kang Kyu Lee <kangkyu1111@gmail.com>
2014-08-01 22:25:17 -0700

    another try with session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  
+
   def new
   end
 
@@ -16,6 +16,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, notice: "Logged out "
+    redirect_to tasks_url, notice: "Logged out "
   end
 end
