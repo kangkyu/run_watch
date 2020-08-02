72c1f5c4cd365ba8c7bfaa1933446619659a8662
Kang-Kyu Lee <kangkyu1111@gmail.com>
2020-08-02 13:30:43 -0700

    Use named scope instead of class method for chaining

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,17 +4,18 @@ class Task < ActiveRecord::Base
 
   validates :date, presence: true
 
+  scope :no_complete, -> (user_id) do
+    complete_ids = Status.where(user_id: user_id, completed: false).pluck(:task_id)
+    where(id: complete_ids)
+  end
+
   def completed_by?(user_id)
     statuses.find_by(user_id: user_id).completed
   end
 
   def status_toggled_for(user_id)
-    status = statuses.where(user_id: user_id).take
+    status = statuses.find_by(user_id: user_id)
     status.toggle!
     status
-  end
-
-  def self.no_complete(user_id)
-    where id: Status.where(user_id: user_id, completed: false).pluck(:task_id)
   end
 end
