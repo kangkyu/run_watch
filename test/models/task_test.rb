81947c11e564c99979d01969dc02b7615d1fe8a9
webkay <websitekay@gmail.com>
2021-09-04 23:05:51 -0700

    Begin testing

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -2,7 +2,14 @@ require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
 
-  def test_the_truth
-    assert true
+  def test_does_not_save_without_date
+    task = Task.new
+    assert_not task.save
+  end
+
+  def test_has_users
+    start = Date.new(2010,7,11)
+    task = Task.new(date: start)
+    assert_respond_to task, :users
   end
 end
